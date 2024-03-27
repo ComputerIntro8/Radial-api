7b4fc290c081a07be870d474b60e68755e5b533b
sangil6372 <sangil6372@naver.com>
2024-03-27 15:34:16 +0900

    first-commit

---
+++ src/main/java/com/example/radialapi/RadialApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.radialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadialApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/radialapi/RadialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.radialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
